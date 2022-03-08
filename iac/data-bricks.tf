614edb6a334f2bf115a94da543cc15c37c6525d1
Michael Griehm <griehmmj@gmail.com>
2022-03-08 17:56:24 -0500

    Added a managed resource group.

--- iac/data-bricks.tf
+++ iac/data-bricks.tf
@@ -1,7 +1,8 @@
-resource "azurerm_databricks_workspace" "example" {
-  name                = local.fqrn
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
-  sku                 = "standard"
-  tags                = var.tags
+resource "azurerm_databricks_workspace" "dbx" {
+  name                        = local.fqrn
+  resource_group_name         = data.azurerm_resource_group.rg.name
+  location                    = data.azurerm_resource_group.rg.location
+  sku                         = "standard"
+  tags                        = var.tags
+  managed_resource_group_name = "${data.azurerm_resource_group.rg}-managed"
 }
