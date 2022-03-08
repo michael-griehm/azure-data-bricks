6bf608618d444fd0d453a863837c39c1c9ebd85e
Michael Griehm <griehmmj@gmail.com>
2022-03-08 17:58:14 -0500

    Fixed managed_resource_group_name.

--- iac/data-bricks.tf
+++ iac/data-bricks.tf
@@ -4,5 +4,5 @@ resource "azurerm_databricks_workspace" "dbx" {
   location                    = data.azurerm_resource_group.rg.location
   sku                         = "standard"
   tags                        = var.tags
-  managed_resource_group_name = "${data.azurerm_resource_group.rg}-managed"
+  managed_resource_group_name = "${data.azurerm_resource_group.rg.name}-managed"
 }
