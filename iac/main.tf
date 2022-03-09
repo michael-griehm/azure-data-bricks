8e6e1686cc52fb433efbba7c02312d092c0c43c2
Michael Griehm <griehmmj@gmail.com>
2022-03-09 14:30:13 -0500

    Updated for changes to the landing zone and improved DRY in github action.

--- iac/main.tf
+++ iac/main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
 }
 
 variable "app_name" {
-  default   = "bricks"
+  default   = "dbx"
   type      = string
   sensitive = false
 }
@@ -42,7 +42,7 @@ locals {
   a_name         = replace(var.app_name, "-", "")
   fqrn           = "${var.app_name}-${var.env}-${local.loc}"
   fqrn_no_dashes = "${local.a_name}-${var.env}-${local.loc}"
-  rg_name        = "rg-${local.fqrn}"
+  rg_name        = "${local.fqrn}"
 }
 
 data "azurerm_client_config" "current" {}
