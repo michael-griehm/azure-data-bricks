ac5428b6b7373dd8a0135a678db09337ad1060e4
Michael Griehm <griehmmj@gmail.com>
2022-03-17 14:55:12 -0400

    adding back storage account management in the dbx resource block

--- iac/data-bricks.tf
+++ iac/data-bricks.tf
@@ -123,6 +123,8 @@ resource "azurerm_databricks_workspace" "dbx" {
   custom_parameters {
     virtual_network_id                                   = azurerm_virtual_network.vnet.id
     vnet_address_prefix                                  = "10.139"
+    storage_account_name                                 = local.fqrn_condensed
+    storage_account_sku_name                             = "Standard_ZRS"
     public_subnet_network_security_group_association_id  = azurerm_subnet_network_security_group_association.public_subnet_nsg.id
     private_subnet_network_security_group_association_id = azurerm_subnet_network_security_group_association.private_subnet_nsg.id
     public_subnet_name                                   = local.public_subnet
